9195c18a4e9f074872e7a44400676d0c79460405
selehadin-cyber <sele4416@gmail.com>
2022-01-11 21:20:54 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom"
 import './App.css';
+import { SliderData } from "./components/ImageData";
+import { ImageSlider } from "./components/ImageSlider";
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        
+        <Routes>
+        <Route path='/' exact element={<ImageSlider slides={SliderData}/>} />
+        </Routes>
+
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/ImageData.js
@@ -0,0 +1,19 @@
+export const SliderData = [
+    
+    {
+      image:
+        'https://i.pinimg.com/originals/86/51/d5/8651d537f4a19468a7972ba993a75edb.jpg'
+    },
+    {
+      image:
+        'https://www.theconstructionindex.co.uk/assets/news_articles/2019/06/1560931186_19jun19-brymec-n41a6270.jpg'
+    },
+    {
+      image:
+        'https://static.independent.co.uk/s3fs-public/thumbnails/image/2014/01/23/19/36-charity-afpgt.jpg'
+    },
+    {
+      image:
+        'http://deshabillemagazine.com/wp-content/uploads/2016/05/FCC-IDP-69.jpg'
+    }
+  ];
