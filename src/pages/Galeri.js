07e602b2c8190792238a4408f3d02d268aea6bd5
selehadin-cyber <sele4416@gmail.com>
2022-01-16 12:57:23 +0300

    gallery place holder commit

--- src/pages/Galeri.js
+++ src/pages/Galeri.js
@@ -6,30 +6,30 @@ export default function Galeri() {
         <div className='cards'>
         <h1 className="center-text">Galeri</h1>
         <div className="container" id="qu-me">
-          <div className="project"><img src="https://picsum.photos/200/300" className="project-image" alt="" /></div>
-          <div className="project"><img src="https://picsum.photos/200/300" className="project-image" alt="" /></div>
-          <div className="project"><img src="https://picsum.photos/200/300" className="project-image" alt="" /></div>
-          <div className="project"><img src="https://picsum.photos/200/300" className="project-image" alt="" /></div>
-          <div className="project"><img src="https://picsum.photos/200/300" className="project-image" alt="" /></div>
-          <div className="project"><img src="https://picsum.photos/200/300" className="project-image" alt="" /></div>
+          <div className="project"><img src={require('../pictures/DSC_0002.JPG')} className="project-image" alt="" /></div>
+          <div className="project"><img src={require('../pictures/DSC_0002.JPG')} className="project-image" alt="" /></div>
+          <div className="project"><img src={require('../pictures/DSC_0002.JPG')} className="project-image" alt="" /></div>
+          <div className="project"><img src={require('../pictures/DSC_0002.JPG')} className="project-image" alt="" /></div>
+          <div className="project"><img src={require('../pictures/DSC_0002.JPG')} className="project-image" alt="" /></div>
+          <div className="project"><img src={require('../pictures/DSC_0002.JPG')} className="project-image" alt="" /></div>
         </div>
         <h1 className="center-text">Orphan Support</h1>
         <div className="container" id="or-su">
-          <div className="project"><img src="https://picsum.photos/200/300" className="project-image" alt="" /></div>
-          <div className="project"><img src="https://picsum.photos/200/300" className="project-image" alt="" /></div>
-          <div className="project"><img src="https://picsum.photos/200/300" className="project-image" alt="" /></div>
-          <div className="project"><img src="https://picsum.photos/200/300" className="project-image" alt="" /></div>
-          <div className="project"><img src="https://picsum.photos/200/300" className="project-image" alt="" /></div>
-          <div className="project"><img src="https://picsum.photos/200/300" className="project-image" alt="" /></div>
+          <div className="project"><img src={require('../pictures/DSC_0002.JPG')} className="project-image" alt="" /></div>
+          <div className="project"><img src={require('../pictures/DSC_0002.JPG')} className="project-image" alt="" /></div>
+          <div className="project"><img src={require('../pictures/DSC_0002.JPG')} className="project-image" alt="" /></div>
+          <div className="project"><img src={require('../pictures/DSC_0002.JPG')} className="project-image" alt="" /></div>
+          <div className="project"><img src={require('../pictures/DSC_0002.JPG')} className="project-image" alt="" /></div>
+          <div className="project"><img src={require('../pictures/DSC_0002.JPG')} className="project-image" alt="" /></div>
         </div>
         <h1 className="center-text">Mosquees</h1>
         <div className="container" id="mo">
-          <div className="project"><img src="https://picsum.photos/200/300" className="project-image" alt="" /></div>
-          <div className="project"><img src="https://picsum.photos/200/300" className="project-image" alt="" /></div>
-          <div className="project"><img src="https://picsum.photos/200/300" className="project-image" alt="" /></div>
-          <div className="project"><img src="https://picsum.photos/200/300" className="project-image" alt="" /></div>
-          <div className="project"><img src="https://picsum.photos/200/300" className="project-image" alt="" /></div>
-          <div className="project"><img src="https://picsum.photos/200/300" className="project-image" alt="" /></div>
+          <div className="project"><img src={require('../pictures/DSC_0002.JPG')} className="project-image" alt="" /></div>
+          <div className="project"><img src={require('../pictures/DSC_0002.JPG')} className="project-image" alt="" /></div>
+          <div className="project"><img src={require('../pictures/DSC_0002.JPG')} className="project-image" alt="" /></div>
+          <div className="project"><img src={require('../pictures/DSC_0002.JPG')} className="project-image" alt="" /></div>
+          <div className="project"><img src={require('../pictures/DSC_0002.JPG')} className="project-image" alt="" /></div>
+          <div className="project"><img src={require('../pictures/DSC_0002.JPG')} className="project-image" alt="" /></div>
         </div>
       </div>
     );
